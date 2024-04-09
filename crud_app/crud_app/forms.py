e3d41dca28079c9a0f2e65c64fce48b26b13c70e
aslin-r <aslinjoelr123@gmail.com>
2024-04-09 21:48:33 +0530

    checkbox is issue

--- crud_app/crud_app/forms.py
+++ crud_app/crud_app/forms.py
@@ -6,4 +6,8 @@ from .models import Register
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = Register
-        fields = ['username', 'email','phone','role','comments','celebrities']  # Add more fields as needed
+        fields = ['username', 'email', 'phone', 'role', 'comments', 'celebrities']
+        widgets = {
+            'email': forms.EmailInput(attrs={'required': False}),
+            'comments': forms.Textarea(attrs={'required': False}),
+        }
--- crud_app/crud_app/models.py
+++ crud_app/crud_app/models.py
@@ -3,9 +3,9 @@
 from django.db import models
 
 class Register(models.Model):
-    username=models.TextField(max_length=100)
-    email=models.EmailField(max_length=100)
-    phone=models.PositiveIntegerField()
-    role=models.TextField()
-    comments=models.TextField(max_length=1000)
-    celebrities=models.TextField()
+    username = models.TextField(max_length=100)
+    email = models.EmailField(null=True, blank=True)  # Allow null values
+    phone = models.PositiveIntegerField()
+    role = models.TextField()
+    comments = models.TextField(null=True, blank=True, max_length=1000)  # Allow null values
+    celebrities = models.TextField()
